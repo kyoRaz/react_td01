b61e51dae834d1450b6d54a7529be15d85ce5af5
Kyo Steeve <80814641+kyoRaz@users.noreply.github.com>
2025-04-07 13:43:40 +0300

    TD03 : Afficher les détails d'une note

--- td01/src/App.jsx
+++ td01/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import logo from './assets/mbds.jpeg';
 import './App.css'
-
+import data from '../../data.json';
 
 
 
@@ -24,6 +24,54 @@ function Header(){
   );
 }
 
+const trierListe = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));  
+    [array[i], array[j]] = [array[j], array[i]]; 
+  }
+  return array;
+};
+
+const getDataTrier =() =>{
+  let dataTrier=trierListe(data);
+  return dataTrier ;
+}
+
+const Element = () => {
+  let data = getDataTrier();
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Unique ID</th>
+            <th>Course</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Student ID</th>
+            <th>Date</th>
+            <th>Grade</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((elt, index) => (
+            <tr key={index}>
+              <td>{elt.unique_id}</td>
+              <td>{elt.course}</td>
+              <td>{elt.student.firstname}</td>
+              <td>{elt.student.lastname}</td>
+              <td>{elt.student.id}</td>
+              <td>{elt.date}</td>
+              <td>{elt.grade}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
 const MainContent = () => {
 
   const now = new Date();
@@ -40,9 +88,14 @@ const MainContent = () => {
   return (
     <main className="main-content">
       <p>Bonjour, on est le {date} et il est {time}</p>
+      <br />
+      <Element></Element>
     </main>
   );
 };
+
+
+
 
 
 const Footer = () => {
